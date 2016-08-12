fe253d93559ab378e0737a5812004b6b647c1cc3
Yuri Gorokhov <yurigorokhov@gmail.com>
2016-08-11 23:58:08 -0700

    Adding code to run motor for 5 seconds to find-megapi

--- find-megapi/find-megapi.go
+++ find-megapi/find-megapi.go
@@ -3,13 +3,28 @@ package main
 import (
 	"fmt"
 	"github.com/yurigorokhov/go-megapi"
+	"time"
 )
 
 func main() {
 	dev, err := megapi.Find_megapi_usb_device()
 	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Found device: {}", dev)
+		panic(err)
+	}
+	fmt.Println("Found device: {}", dev)
+	megaPi, err := megapi.NewMegaPi(dev)
+	if err != nil {
+		panic(err)
+	}
+	defer megaPi.Close()
+	fmt.Println("Running motor on port 1 for 5 seconds")
+	err = megaPi.MotorRun(1, 50)
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(5 * time.Second)
+	err = megaPi.MotorRun(1, 0)
+	if err != nil {
+		panic(err)
 	}
 }
